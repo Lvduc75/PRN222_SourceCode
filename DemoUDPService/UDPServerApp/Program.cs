506e5487c0c756917298451bdd603d4777d57837
Lvduc75 <luuvietduc75@gmail.com>
2025-01-12 23:20:39 +0700

    Add DemoUDP solution ( LAB2 )

---
+++ DemoUDPService/UDPServerApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UDPServerApp
+{
+    class Program
+    {
+        const int listenPort = 11000;
+        const string host = "127.0.0.1";
+        private static void StartListeneer()
+        {
+            String message;
+            UdpClient listener = new UdpClient(listenPort);
+            IPAddress address = IPAddress.Parse(host);
+            IPEndPoint remoteEndpoint = new IPEndPoint(address, listenPort);
+            Console.Title = "UDP Server";
+            Console.WriteLine(new string('*', 40));
+            try
+            {
+                while (true)
+                {
+                    Console.WriteLine("Waiting for broadcast");
+                    byte[] bytes = listener.Receive(ref remoteEndpoint);
+                    message = Encoding.ASCII.GetString(bytes, 0, bytes.Length);
+                    Console.WriteLine($"Received broadcast from: {remoteEndpoint}:{message}");
+                }
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                listener.Close();
+            }
+        }
+        public static void Main(string[] args)
+        {
+            Thread listenerThread = new Thread(StartListeneer);
+            listenerThread.Start();
+        }
+    }
+}
