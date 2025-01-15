6a019f7e4bb01120032b2a135e346813d77adfa8
Lvduc75 <luuvietduc75@gmail.com>
2025-01-15 23:32:42 +0700

    Add: Chapter2: TaskDemostration 01, 02, 03

---
+++ Chapter2/TaskDemostration01/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace TaskDemostration01
+{
+    class Program
+    {
+        static void PrintNumber(string message)
+        {
+            for (int i = 0; i <= 5; i++)
+            {
+                Console.WriteLine($"{message}:{i}");
+                Thread.Sleep(1000);
+            }
+        }
+        static void Main()
+        {
+            Thread.CurrentThread.Name = "Main";
+            // Create a task by using lamda expression
+            Task task01 = new Task(() => PrintNumber("Task01"));
+            task01.Start();
+            // Create a task by using delegate and run the task
+            Task task02 = Task.Run(delegate { PrintNumber("Task02"); });
+            // Create a task by using a Action delegate
+            Task task03 = new Task(new Action(() => { PrintNumber("Task03"); }));
+            task03.Start();
+            Console.WriteLine($"Thread '{Thread.CurrentThread.Name}'");
+            Console.ReadKey();
+        }
+    }
+}
--- DemoTCPService/ServerApp/obj/Debug/net8.0/ServerApp.AssemblyInfo.cs
+++ DemoTCPService/ServerApp/obj/Debug/net8.0/ServerApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServerApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+027ebd069b33cc245248cad74e0e396b6b1eece7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServerApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServerApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
