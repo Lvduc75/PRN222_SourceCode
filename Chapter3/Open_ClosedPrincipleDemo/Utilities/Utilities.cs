5a4aeb98513b006ad2e2c96fc1e8004f4a84d5b6
Lvduc75 <luuvietduc75@gmail.com>
2025-02-06 00:13:43 +0700

    Add chapter3: O Princple

---
+++ Chapter3/Open_ClosedPrincipleDemo/Utilities/Utilities.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using SingleResponsibilityPrincipleDemo.Model;
+using System.Collections.Generic;
+using System.IO;
+
+internal class Utilities
+{
+    static string ReadFile(string filename)
+    {
+        return File.ReadAllText(filename);
+    }
+
+    internal static List<Book> ReadData()
+    {
+        var filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", "..", "..", "Data", "BookStore1.json");
+        var cadJSON = File.ReadAllText(filePath);
+        return JsonConvert.DeserializeObject<List<Book>>(cadJSON);
+    }
+
+    internal static List<Book> ReadDataExtra()
+    {
+        List<Book> books = ReadData();
+        var filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", "..", "..", "Data", "BookStore2.json");
+        var cadJSON = File.ReadAllText(filePath);
+        books.AddRange(JsonConvert.DeserializeObject<List<Book>>(cadJSON));
+        return books;
+    }
+}
